18b34a6412c856348e320d3500c06bd4a215a33b
Pavel Panchekha <pavpanchekha@gmail.com>
2013-05-13 18:34:44 -0400

    Added License information

--- dropbox.el
+++ dropbox.el
@@ -4,15 +4,31 @@
 ;;           (C) 2013 Drew Haven      <ahaven@alum.mit.edu>
 
 ;; Author: Pavel Panchekha <me@pavpanchekha.com>
-;; Author: Drew Haven      <ahaven@alum.mit.edu>
 ;; Version: 0.9
 ;; Package-Requires: ((json "1.2") (oauth "1.0.3"))
 ;; Keywords: dropbox
+;; Contributors:
+;;     Drew Haven      <ahaven@alum.mit.edu>
+
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
 ;; This package allows one to access files stored in Dropbox,
 ;; effectively acting as an Emacs Dropbox client and SDK.
+
+(provide 'dropbox)
 
 ;;; TODO
 ;; - Return permissions other than -rwx------ if folder has shares
